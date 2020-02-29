67f8bdc6ef76f00e291abe8006d16c16db6df548
Adam Young <adamyoungg97@gmail.com>
2020-02-29 13:46:49 +0000

    Added controller endpoints and API docs

---
+++ OpenTransportData.API/Controllers/TrainController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenTransportData.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainController : ControllerBase
+    {
+        [HttpGet("{crs}/arrivals")]
+        public IActionResult GetArrivals(string crs)
+        {
+            return Ok("Arrivals");
+        }
+
+        [HttpGet("{crs}/departures")]
+        public IActionResult GetDepartures(string crs)
+        {
+            return Ok("Departures");
+        }
+
+        [HttpGet("stations")]
+        public IActionResult GetStations()
+        {
+            return Ok();
+        }
+    }
+}
